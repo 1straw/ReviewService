4ff85090f9970feb1adfb03aade39e6b1de65d2f
Rammsterr <94johram@gafe.molndal.se>
2025-04-14 12:17:45 +0200

    Update ProductRepository.java

--- ReviewService/src/main/java/se/reviewservice/repository/ProductRepository.java
+++ ReviewService/src/main/java/se/reviewservice/repository/ProductRepository.java
@@ -2,11 +2,11 @@ package se.reviewservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.reviewservice.model.Customer;
+import se.reviewservice.model.Product;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository <ProductRepository, String> {
+public interface ProductRepository extends JpaRepository <Product, String> {
     List<ProductRepository> findByCustomer(Customer customer);
     boolean existsByProductId(String productId); // Kollar om en produkt redan finnns
-
 }
